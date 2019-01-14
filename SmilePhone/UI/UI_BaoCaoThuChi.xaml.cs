a69b0b39fe35d84ba5a9e3ab50c4010fce0f3162
longdang0497 <danghoanglong3004@gmail.com>
2019-01-15 01:21:22 +0700

    Xong UI báo cáo thu chi

--- SmilePhone/UI/UI_BaoCaoThuChi.xaml.cs
+++ SmilePhone/UI/UI_BaoCaoThuChi.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BUS;
+using DTO;
 
 namespace SmilePhone.UI
 {
@@ -20,11 +22,70 @@ namespace SmilePhone.UI
     /// </summary>
     public partial class UI_BaoCaoThuChi : UserControl
     {
-        private Grid grid;
+        public Grid gridReport;
         public UI_BaoCaoThuChi(Grid gridReport)
         {
             InitializeComponent();
-            grid = gridReport;
+            this.gridReport = gridReport;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnThem_Click(object sender, RoutedEventArgs e)
+        {
+            //UserControl usc = new UI_ThemBaoCaoTK(gridReport, dgvStockReport.SelectedItem as DTO_BaoCaoTonKho);
+            //gridReport.Children.Clear();
+            //gridReport.Children.Add(usc);
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            //UserControl usc = new UI_ThemNhanVien(gridMain, dgvEmployees.SelectedItem as DTO_NhanVien);
+            //gridMain.Children.Clear();
+            //gridMain.Children.Add(usc);
+        }
+
+        private void btnTim_Click(object sender, RoutedEventArgs e)
+        {
+            //UserControl usc = new UI_ThemNhanVien(gridMain, dgvEmployees.SelectedItem as DTO_NhanVien);
+            //gridMain.Children.Clear();
+            //gridMain.Children.Add(usc);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //UserControl usc = new UI_ThemNhanVien(gridMain, dgvEmployees.SelectedItem as DTO_NhanVien);
+            //gridMain.Children.Clear();
+            //gridMain.Children.Add(usc);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBoxResult result = MessageBox.Show("Bạn có muốn xóa dòng này?", "Confirmation", MessageBoxButton.YesNo);
+            //if (result == MessageBoxResult.Yes)
+            //{
+            //    if (dgvStockReport.SelectedItem != null)
+            //    {
+            //        DTO_BaoCaoTonKho obj = dgvStockReport.SelectedItem as DTO_BaoCaoTonKho;
+            //        BUS_BaoCaoTonKho.Instance.Delete(obj);
+            //        dgvStockReport.ItemsSource = BUS_BaoCaoTonKho.showData();
+            //    }
+            //}
+        }
+
+        private void TxtSearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //FindListViewItem(dgvEmployees);
+        }
+
+        private void btnChart_Click(object sender, RoutedEventArgs e)
+        {
+            //UserControl usc = new UI_BieuDoBaoCao(gridReport);
+            //gridReport.Children.Clear();
+            //gridReport.Children.Add(usc);
         }
     }
 }
